976a26a71098a42c1d1846a181d7b129d4505206
Riley Mathews <rileymathews80@gmail.com>
2018-04-19 11:30:18 -0500

    finished object representing advisor and portfolio

---
+++ advisor.js
@@ -0,0 +1,77 @@
+const advisor = Object.create({}, {
+    company: {
+        value: "Finance",
+        enumerable: true,
+        writable: true
+    },
+    specialty: {
+        value: "technology",
+        enumerable: true,
+        writable: true
+    },
+    name: {
+        value: "Charles",
+        enumerable: true,
+        writable: false
+    },
+    worth: {
+        value: function () {
+            let worth = 0
+            for (let key in this.portfolio) {
+                console.log(key)
+                worth += this.portfolio[key].valuation
+            }
+            return worth
+        }
+    },
+    purchase: {
+        value: function (stockToChange, purchaseQuantity, pricePerStock) {
+            let stockFound = false
+            for (let stock in this.portfolio) {
+                if (stock === stockToChange) {
+                    this.portfolio[stock].shares += purchaseQuantity
+                    this.portfolio[stock].valuation += purchaseQuantity * pricePerStock
+                    stockFound = true
+                    return stock
+                }
+            };
+            if (stockFound === false) {
+                this.portfolio[stockToChange] = {
+                    "shares": purchaseQuantity,
+                    "valuation": pricePerStock * purchaseQuantity
+                }
+            }
+        },
+        enumerable: false
+    }, //ends purchase property
+    sell: {
+        value: function (stockSold, quantitySold, pricePerStock) {
+            console.log('started function')
+            for (let stock in this.portfolio) {
+                console.log(this.portfolio[stock])
+                if (stock === stockSold) {
+                    this.portfolio[stock].shares -= quantitySold
+                    this.portfolio[stock].valuation -= quantitySold * pricePerStock
+                    if (this.portfolio[stock].shares === 0) {
+                        delete this.portfolio[stock]
+                    }
+                }
+            }
+        }
+    }
+}) //ends object creation
+
+advisor.portfolio = {
+    MSFT: {
+        shares: 10,
+        valuation: 100000
+    }
+}
+
+advisor.purchase("MSFT", 100, 100)
+advisor.sell("MSFT", 5, 50)
+advisor.purchase("APPL", 50, 100)
+advisor.sell("APPL", 30, 100)
+
+console.log(advisor.portfolio)
+console.log(advisor.worth())
